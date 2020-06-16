0a65a2cd9e20c148f5509f30fc044c7421026831
Devesh Todarwal <todarwal.devesh@gmail.com>
2020-06-16 12:54:39 +0530

    Inital Search/Sort/Graph Algos

---
+++ Graph Algorithms/FordFulkerson.py
@@ -0,0 +1,24 @@
+def max_flow(cap, s ,t):
+
+  def augment_path(cap, visited, i, t):
+
+    if i==t
+      return True
+    visited[i]=True
+    for j in range(len(cap)):
+      if not visted[j] and cap [i][j]>0 and augment_path(cap, visited, j, t):
+        cap[i][j] -= 1
+        cap[j][i] += 1
+        return True
+      return False
+
+  flow=0
+  while True:
+    if not augment_path(cap, [False]*len(cap), s, t):
+      return max_flow += 1
+
+def test():
+  capacity = [[0,1,1,0],[1,0,1,1],[1,1,0,1],[0,1,1,0]]
+  assert max_flow(capacity, 0, 3) == 2
+
+test()
